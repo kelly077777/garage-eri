ed805edfce3d901094951e3dbaff4e4398eb942c
kelly077777 <shemakelly7@gmail.com>
2026-02-23 09:19:02 +0200

    Update API URL for production

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:8080/api' });
+const api = axios.create({ baseURL: 'garage-eri-production.up.railway.app' });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
